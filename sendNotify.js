6d5f6fe91fb622387e20fe6823f308e1a69b7894
huangjj <huangjj>
2021-07-14 16:01:31 +0800

    fix

--- sendNotify.js
+++ sendNotify.js
@@ -161,13 +161,13 @@ async function sendNotify(text, desp, params = {}, author = '') {
   //提供6种通知
   desp += author;//增加作者信息，防止被贩卖等
   await Promise.all([
-    //serverNotify(text, desp),//微信server酱
+    serverNotify(text, desp),//微信server酱
     pushPlusNotify(text, desp), //pushplus(推送加)
-    //qmsgNotify(text+'\n'+desp)
+    qmsgNotify(text+'\n'+desp)
   ])
   //由于上述两种微信通知需点击进去才能查看到详情，故text(标题内容)携带了账号序号以及昵称信息，方便不点击也可知道是哪个京东哪个活动
   //text = text.match(/.*?(?=\s?-)/g) ? text.match(/.*?(?=\s?-)/g)[0] : text;
-  /*await Promise.all([
+  await Promise.all([
     BarkNotify(text, desp, params),//iOS Bark APP
     tgBotNotify(text, desp),//telegram 机器人
     ddBotNotify(text, desp),//钉钉机器人
@@ -175,7 +175,7 @@ async function sendNotify(text, desp, params = {}, author = '') {
     qywxamNotify(text, desp), //企业微信应用消息推送
     iGotNotify(text, desp, params),//iGot
     //CoolPush(text, desp)//QQ酷推
-  ])*/
+  ])
 }
 
 function serverNotify(text, desp, time = 2100) {
