e973dc556c8447dfd3f8c393a6878d1367b0480f
huangjj <huangjj>
2021-07-15 17:53:28 +0800

    fix

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -148,6 +148,8 @@
 45 */2 * * * node /scripts/jd_joy.js >> /scripts/logs/jd_joy.log 2>&1
 # 宠汪汪偷狗粮
 10 0-21/3 * * * node /scripts/jd_joy_steal.js >> /scripts/logs/jd_joy_steal.log 2>&1
+# 宠汪汪兑换
+0 0,8,12,16 * * * node /scripts/jd_joy_reward.js >> /scripts/logs/jd_joy_reward.log 2>&1
 
 #chb
 #0 0 * * * node /scripts/kj.js >> /scripts/logs/kj.log 2>&1
